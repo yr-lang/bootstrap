0c55c772e5793beebb67127da3a22903e24e6dbc
mwtheus <mwtheus.sh@gmail.com>
2026-07-15 10:16:51 -0300

    updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,10 +10,12 @@ if [ "$1" != "--skip-user" ] && [ "$USER" != "$USER_NAME" ]; then
     sudo passwd "$USER_NAME"
   }
 
-  exec sudo -u "$USER_NAME" -H bash -c "
+  sudo -iu "$USER_NAME" bash -c "
     cd ~
-    $(curl -fsSL https://raw.githubusercontent.com/yr-lang/bootstrap/main/bootstrap.sh)
-  " -- --skip-user
+    curl -fsSL https://raw.githubusercontent.com/yr-lang/bootstrap/main/bootstrap.sh | bash -s -- --skip-user
+  "
+
+  exec su - "$USER_NAME"
 fi
 
 BOOTSTRAP_DIR="$HOME/.yrkit-bootstrap"
